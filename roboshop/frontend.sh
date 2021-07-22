aba9cf07636b04c74a4be95a9d08f0be7a2ae0d8
Venkat <reachvenkatas@gmail.com>
2021-07-22 15:49:36 -0700

    roboshop

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -16,7 +16,6 @@ fi
 #step 1:outputs from the commands should not display on the screen
 #step 2: validate the command executed fine or not($?)
 #STep 3: Need to check the script is running as root user id or not
-
 echo -e "Enabling the  Nginx\t\t\t..."
 systemctl enable nginx &>>$LOG
 if  [ $? -eq 0 ]; then
