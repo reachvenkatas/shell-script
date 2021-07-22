0b9a26acae8f8efdd25d007565c0091002b4212d
Venkat <reachvenkatas@gmail.com>
2021-07-22 15:44:27 -0700

    roboshop

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -6,7 +6,7 @@ rm -f $LOG
 echo -n -e "Installing Nginx\t\t\t..."
 #Installing the NGinx and Redirecting the output to the log file
 yum install nginx -y &>>$LOG
-if[$? -eq 0];then
+if[$? -eq 0]; then
   echo -e "\e[32mdone\e[0m"
 else
   echo -e "\e[31mfail\e[0m"
@@ -19,7 +19,7 @@ fi
 
 echo -e "Enabling the  Nginx\t\t\t..."
 systemctl enable nginx &>>$LOG
-if[$? -eq 0];then
+if[$? -eq 0]; then
   echo -e "\e[32mdone\e[0m"
 else
   echo -e "\e[31mfail\e[0m"
@@ -28,7 +28,7 @@ fi
 
 echo -e "Starting the  Nginx\t\t\t..."
 systemctl start nginx &>>$LOG
-if[$? -eq 0];then
+if[$? -eq 0]; then
   echo -e "\e[32mdone\e[0m"
 else
   echo -e "\e[31mfail\e[0m"
