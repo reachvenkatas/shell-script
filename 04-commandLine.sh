cfd9ce51bbf5c5518e1679d2375c7628297c0b98
Venkat <reachvenkatas@gmail.com>
2021-07-21 19:53:20 -0700

    functions

--- 04-commandLine.sh
+++ 04-commandLine.sh
@@ -1,5 +1,5 @@
 sample() {
-  echo "i am in the function\n"
+  echo -e "i am in the function\n"
   a=10
   echo -e "values of a=$a, b=$b, $* \n"
   b=100
@@ -9,7 +9,7 @@ sample() {
 
 b=20
 echo -e "i am in the main function\n"
-echo -e "values of a=$a, b=$b, $* \n"
-sample
+echo -e "values of a=${a}, b=$b, $* \n"
+sample ABC
 a=100
 echo -e "values of a=$a, b=$b, $* \n"
