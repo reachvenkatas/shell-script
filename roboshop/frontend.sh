e58d331b3bb5b3fb449b967fc310e7f7c0f95ae2
Venkat <reachvenkatas@gmail.com>
2021-07-22 15:40:49 -0700

    roboshop

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -2,12 +2,35 @@
 LOG=/tmp/frontend.log
 #mv $LOG /tmp/backup/$LOG_$(date +%F-%T).log
 rm -f $LOG
-echo -e "Installing Nginx\t\t...\t\e[32mdone\e[0m"
+
+echo -n -e "Installing Nginx\t\t\t..."
 #Installing the NGinx and Redirecting the output to the log file
 yum install nginx -y &>>$LOG
+if[if $? -eq 0]
+  echo -e "\e[32mdone\e[0m"
+else
+  echo -e "\e[31mfail\e[0m"
+  exit 1
+fi
 
-echo -e "Enabling the  Nginx\t\t...\t\e[32mdone\e[0m"
+#step 1:outputs from the commands should not display on the screen
+#step 2: validate the command executed fine or not($?)
+#STep 3: Need to check the script is running as root user id or not
+
+echo -e "Enabling the  Nginx\t\t\t..."
 systemctl enable nginx &>>$LOG
+if[if $? -eq 0]
+  echo -e "\e[32mdone\e[0m"
+else
+  echo -e "\e[31mfail\e[0m"
+  exit 1
+fi
 
-echo -e "Starting the  Nginx\t\t...\t\e[32mdone\e[0m"
+echo -e "Starting the  Nginx\t\t\t..."
 systemctl start nginx &>>$LOG
+if[if $? -eq 0]
+  echo -e "\e[32mdone\e[0m"
+else
+  echo -e "\e[31mfail\e[0m"
+  exit 1
+fi
