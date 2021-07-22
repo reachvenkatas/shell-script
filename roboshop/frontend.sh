be866f9d6e6c27bda6bdfa59d03e1b63c4e87ab0
Venkat <reachvenkatas@gmail.com>
2021-07-22 14:54:46 -0700

    roboshop

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -4,10 +4,10 @@ LOG=/tmp/frontend.log
 rm -f $LOG
 echo -e "Installing Nginx\t\t...\t\e[32mdone\e[0m"
 #Installing the NGinx and Redirecting the output to the log file
-yum install nginx -y >>$LOG
+yum install nginx -y &>>$LOG
 
 echo -e "Enabling the  Nginx\t\t...\t\e[32mdone\e[0m"
-systemctl enable nginx >>$LOG
+systemctl enable nginx &>>$LOG
 
 echo -e "Starting the  Nginx\t\t...\t\e[32mdone\e[0m"
-systemctl start nginx >>$LOG
+systemctl start nginx &>>$LOG
