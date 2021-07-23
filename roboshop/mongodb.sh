6d60d54cd92236a5467215ec34be3687dc191546
Venkat <reachvenkatas@gmail.com>
2021-07-22 17:56:57 -0700

    roboshop

---
+++ roboshop/mongodb.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+source common.sh
+PRINT "adding the mongodb repo to yum repo"
+echo '[mongodb-org-4.2]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo &>>$LOG
+statusCheck $?
+
+PRINT "Install Mongo & Start Service"
+yum install -y mongodb-org >>$LOG
+statusCheck $?
+
+#PRINT "update mongodb listen address"
+#Update Liste IP address from 127.0.0.1 to 0.0.0.0 in config file
+#Config file: /etc/mongod.conf
+
+#then restart the service
+
+# systemctl restart mongod
+#Every Database needs the schema to be loaded for the application to work.
+#Download the schema and load it.
+
+# curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+
+# cd /tmp
+# unzip mongodb.zip
+# cd mongodb-main
+# mongo < catalogue.js
+# mongo < users.js
+
+#PRINT "Install Mongo & Start Service"
+# systemctl enable mongod >>$LOG
+# systemctl start mongod >>$LOG
