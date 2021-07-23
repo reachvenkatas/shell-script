71e7ef4cd6a923717c261d7271385852e5601eed
Venkat <reachvenkatas@gmail.com>
2021-07-22 18:02:29 -0700

    roboshop

--- roboshop/mongodb.sh
+++ roboshop/mongodb.sh
@@ -6,7 +6,7 @@ name=MongoDB Repository
 baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
 gpgcheck=1
 enabled=1
-gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo &>>$LOG
+gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
 statusCheck $?
 
 PRINT "Install Mongo & Start Service"
