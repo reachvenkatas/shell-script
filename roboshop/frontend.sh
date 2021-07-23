94c3e3ded21fd6a452f5d40306a6602da9acb355
Venkat <reachvenkatas@gmail.com>
2021-07-22 17:12:36 -0700

    roboshop

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -13,3 +13,23 @@ statusCheck $?
 PRINT "Starting the  Nginx"
 systemctl start nginx &>>$LOG
 statusCheck $?
+
+PRINT "download static content - front end"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG
+statusCheck $?
+
+PRINT "remove the old HTDOCS"
+cd /usr/share/nginx/html &>>$LOG && rm -rf * &>>$LOG
+statusCheck $?
+
+PRINT "Etract HTDOCS files"
+unzip /tmp/frontend.zip &>>$LOG && mv frontend-main/* . &>>$LOG && mv static/* . &>>$LOG && rm -rf frontend-master static &>>$LOG
+statusCheck $?
+
+PRINT "Moves the conf file to nginx location"
+mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG
+statusCheck $?
+
+PRINT "ReStarting the  Nginx"
+systemctl restart nginx &>>$LOG
+statusCheck $?
