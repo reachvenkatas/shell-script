8554de473a315150042563ff789ccc6700f2ef45
Venkat <reachvenkatas@gmail.com>
2021-07-22 22:17:45 -0700

    frontend

--- roboshop/redis.sh
+++ roboshop/redis.sh
@@ -10,9 +10,9 @@ yum install redis -y --enablerepo=remi &>>$LOG
 statusCheck $?
 
 PRINT "update redis Listen Address"
-sed -i -e '/s/127.0.0.1/0.0.0.0/' /etc/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf
 statusCheck $?
 
 Start Redis Database
-systemctl enable redis &>>$LOG && systemctl start redis &>>$LOG
+systemctl enable redis &>>$LOG && systemctl restart redis &>>$LOG
 statusCheck $?
