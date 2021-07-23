4a24e21fb4d039670fa57080dda909aac0aaad21
Venkat <reachvenkatas@gmail.com>
2021-07-23 08:26:38 -0700

    user

--- roboshop/common.sh
+++ roboshop/common.sh
@@ -55,7 +55,7 @@ chown roboshop:roboshop /home/roboshop -R &>>$LOG
 statusCheck $?
 
 PRINT "Update SystemD File"
-sed -i -e "s/MONGO_DNSNAME/mongodb.roboshop.internal/" "s/REDIS_ENDPOINT/redis.roboshop.internal/" "s/MONGO_ENDPOINT/mongodb.roboshop.internal/"/home/roboshop/${COMPONENT}/systemd.service && mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service &>>$LOG
+sed -i -e "s/MONGO_DNSNAME/mongodb.roboshop.internal/" -e "s/REDIS_ENDPOINT/redis.roboshop.internal/" -e "s/MONGO_ENDPOINT/mongodb.roboshop.internal/" /home/roboshop/${COMPONENT}/systemd.service && mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service &>>$LOG
 statusCheck $?
 
 PRINT "daemon-reload"
