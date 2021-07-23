0be59c4a8f0b117df8300d020210126880eb06f8
Venkat <reachvenkatas@gmail.com>
2021-07-23 08:57:59 -0700

    user

--- roboshop/mysql.sh
+++ roboshop/mysql.sh
@@ -8,13 +8,13 @@ name=MySQL 5.7 Community Server
 baseurl=http://repo.mysql.com/yum/mysql-5.7-community/el/7/$basearch/
 enabled=1
 gpgcheck=0' > /etc/yum.repos.d/mysql.repo
-STAT_CHECK $?
+StatusCheck $?
 
 
 PRINT "Install MySQL\t\t"
 yum install mysql-community-server -y &>>$LOG
-STAT_CHECK $?
+StatusCheck $?
 
 PRINT "Start MySQL Service\t"
 systemctl enable mysqld &>>$LOG  && systemctl start mysqld &>>$LOG
-STAT_CHECK $?
+StatusCheck $?
