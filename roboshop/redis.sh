c1ae87cc0f5320009bce6f90c2e109b94098183b
Venkat <reachvenkatas@gmail.com>
2021-07-22 22:08:39 -0700

    frontend

---
+++ roboshop/redis.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+source common.sh
+
+PRINT "Install redis repo"
+yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y &>>$LOG
+statusCheck $?
+
+PRINT "Install redis"
+yum install redis -y --enablerepo=remi &>>$LOG
+statusCheck $?
+
+PRINT "update redis Listen Address"
+sed -i -e '/s/127.0.0.1/0.0.0.0/' /etc/redis.conf
+statusCheck $?
+
+Start Redis Database
+systemctl enable redis &>>$LOG && systemctl start redis &>>$LOG
+statusCheck $?
