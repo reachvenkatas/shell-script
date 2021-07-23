28c95c24f44ea4aefa89ffa0896c42956950d120
Venkat <reachvenkatas@gmail.com>
2021-07-22 18:55:48 -0700

    catalouge

--- roboshop/catalouge.sh
+++ roboshop/catalouge.sh
@@ -18,5 +18,5 @@ curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/cat
 statusCheck $?
 
 PRINT "Extract downloaded code"
-cd /home/roboshop && unzip /tmp/catalogue.zip &>>$LOG && mv catalogue-main catalogue && cd /home/roboshop/catalogue && npm install --unsafe-perm &>>$LOG
+cd /home/roboshop && unzip -o /tmp/catalogue.zip &>>$LOG && rm -rf catalogue && mv catalogue-main catalogue && cd /home/roboshop/catalogue && npm install --unsafe-perm &>>$LOG
 statusCheck $?
