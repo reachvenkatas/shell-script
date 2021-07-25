24079bf8684ba61e035eacbfda64142a0c00de75
Venkat <reachvenkatas@gmail.com>
2021-07-25 10:14:25 -0700

    terraform

--- Terraform/03-example/ec2.tf
+++ Terraform/03-example/ec2.tf
@@ -21,7 +21,7 @@ resource "aws_security_group" "allow_ssh" {
 
   egress {
     from_port        = 0
-    to_port          = 655535
+    to_port          = 65535
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
