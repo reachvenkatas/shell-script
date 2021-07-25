fb2d10418faa02bbfe1e86a2249951feee73b4c4
Venkat <reachvenkatas@gmail.com>
2021-07-25 10:23:16 -0700

    terraform

--- Terraform/03-example/ec2.tf
+++ Terraform/03-example/ec2.tf
@@ -1,11 +1,12 @@
-//resource "aws_instance" "sample" {
-//  ami           = ami-074df373d6bafa625
-//  instance_type = "t2.micro"
-//
-//  tags = {
-//    Name = "Test"
-// }
-//}
+resource "aws_instance" "sample" {
+  ami                     = ami-074df373d6bafa625
+  instance_type           = "t2.micro"
+  vpc_security_group_ids  = [aws_security_group.allow_ssh.id]
+
+  tags = {
+    Name = "Test"
+ }
+}
 
 resource "aws_security_group" "allow_ssh" {
   name          = "allow_ssh"
@@ -36,5 +37,8 @@ provider "aws" {
 
 output "sg-attributes" {
   value = aws_security_group.allow_ssh
+}
+output "ec2-attr" {
+  value = aws_instance.sample.public_ip
 }
 
