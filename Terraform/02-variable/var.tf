1b83d7ca918c7f659d4ac339038ebe5d906d9fea
Venkat <reachvenkatas@gmail.com>
2021-07-25 01:58:31 -0700

    rabbitmq

--- Terraform/02-variable/var.tf
+++ Terraform/02-variable/var.tf
@@ -1,12 +1,12 @@
 variable "sample" {
   default = "Hello World"
 }
-output "output" {
-  value   = " var.sample , var.sample1 , var.sample2 ,  ${var.sample}, ${var.sample} , ${var.sample}  "
-}
 variable "sample1" {
   value   = 10
 }
 variable "sample2" {
   value   = True
+}
+output "output" {
+  value   = " var.sample , var.sample1 , var.sample2 ,  ${var.sample}, ${var.sample} , ${var.sample}  "
 }
