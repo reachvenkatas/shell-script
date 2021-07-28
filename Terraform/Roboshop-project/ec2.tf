60352600d21c30e6875d6747fb088befad55d3d9
Venkat <reachvenkatas@gmail.com>
2021-07-28 11:38:11 -0700

    terraform

--- Terraform/Roboshop-project/ec2.tf
+++ Terraform/Roboshop-project/ec2.tf
@@ -19,17 +19,24 @@ resource "aws_ec2_tag" "example" {
   value                 = element(var.COMPONENTS,count.index)
 }
 
-
+resource "aws_route53_record" "records" {
+  count                     = local.LENGTH
+  name                      = element(var.COMPONENTS, count.index)
+  type                      = "A"
+  zone_id                   = "Z08761133VTY76N7SJWCJ"
+  ttl                       = 300
+  records                   = [element(aws_spot_instance_request.cheap_worker.*.private_ip, count.index)]
+}
 
 resource "null_resource" "run_shell_script" {
-  count = local.LENGTH
+  count                     = local.LENGTH
+  depends_on                = [aws_route53_record.records]
   provisioner "remote-exec" {
     connection {
-      host = element(aws_spot_instance_request.cheap_worker.*.public_ip,count.index)
-      user = "centos"
-      password = "DevOps321"
+      host                  = element(aws_spot_instance_request.cheap_worker.*.public_ip,count.index)
+      user                  = "centos"
+      password              = "DevOps321"
     }
-
     inline = [
       "cd /home/centos",
       "git clone https://github.com/reachvenkatas/shell-script.git",
