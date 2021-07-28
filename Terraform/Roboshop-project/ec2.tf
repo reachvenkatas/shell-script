2fa0a2e0f3585d14219af2dc35a1594593bb37ce
Venkat <reachvenkatas@gmail.com>
2021-07-28 10:12:13 -0700

    terraform

--- Terraform/Roboshop-project/ec2.tf
+++ Terraform/Roboshop-project/ec2.tf
@@ -3,7 +3,7 @@ resource "aws_spot_instance_request" "cheap_worker" {
   ami                   = "ami-074df373d6bafa625"
   spot_price            = "0.03"
   spot_type             = "persistent"
-  instance_interruption_behaviour = "stop"
+  instance_interruption_behavior = "stop"
   instance_type         = "t3.micro"
   wait_for_fulfillment  = true
   tags          = {
