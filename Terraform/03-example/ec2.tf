d79452e4a5f33ac2cae6a889fbfd8f40c7086c51
Venkat <reachvenkatas@gmail.com>
2021-07-28 03:42:28 -0700

    terraform

--- Terraform/03-example/ec2.tf
+++ Terraform/03-example/ec2.tf
@@ -8,6 +8,15 @@ resource "aws_instance" "sample" {
  }
 }
 
+resource "aws_instance" "sample1" {
+  ami                     = "ami-074df373d6bafa625"
+  instance_type           = "t2.micro"
+  vpc_security_group_ids  = [aws_security_group.allow_ssh.id]
+
+  tags = {
+    Name = "Test1"
+  }
+}
 
 resource "aws_security_group" "allow_ssh" {
   name          = "allow_ssh"
