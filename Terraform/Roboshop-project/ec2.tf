0bc26e6d247392754d86914604fb4d53952f77f7
Venkat <reachvenkatas@gmail.com>
2021-07-28 10:26:55 -0700

    terraform

--- Terraform/Roboshop-project/ec2.tf
+++ Terraform/Roboshop-project/ec2.tf
@@ -10,3 +10,10 @@ resource "aws_spot_instance_request" "cheap_worker" {
     Name        = "${element(var.COMPONENTS,count.index)}-${count.index}-devops57"
   }
 }
+
+resource "aws_ec2_tag" "example" {
+  count                 = length(var.COMPONENTS)
+  resource_id           = element(aws_spot_instance_request.cheap_worker.*.spot_instance_id,count.index)
+  key                   = "Name"
+  value                 = element(var.COMPONENTS,count.index)
+}
