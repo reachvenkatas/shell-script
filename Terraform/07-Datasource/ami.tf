d3b328b0ff94fb90b3861377d62bfa4090cc93d9
Venkat <reachvenkatas@gmail.com>
2021-07-29 09:16:15 -0700

    terraform

--- Terraform/07-Datasource/ami.tf
+++ Terraform/07-Datasource/ami.tf
@@ -4,6 +4,12 @@ data "aws_ami" "example" {
   owners           = ["973714476881"]
 }
 
+data "aws_ec2_spot_price" "example" {
+  instance_type     = "t3.medium"
+  //availability_zone = "us-west-2a"
+
+}
+
 output "image"  {
   value = data.aws_ami.example.id
 }
