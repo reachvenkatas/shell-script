48f55bf2358255209fb5c0a8f1f62a4966317984
Venkat <reachvenkatas@gmail.com>
2021-07-31 05:18:29 -0700

    terraform

--- Terraform/03-example/ec2.tf
+++ Terraform/03-example/ec2.tf
@@ -67,7 +67,7 @@ output "ec2-attr" {
  //   script_sha = "${sha256(file("${path.module}/script.sh"))}"
  // }
 module "my_git_repo" {
-  source = "git::https://github.com/reachvenkatas/shell-script/hello.sh"
+  source = "git::https://github.com/reachvenkatas/shell-script//hello.sh"
 }
 output "myfile" {
   value = "module.my_git_repo.hello.sh"
