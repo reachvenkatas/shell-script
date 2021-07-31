b769b007bc987ada8724a167d6562ddcd3d34b8a
Venkat <reachvenkatas@gmail.com>
2021-07-31 05:53:07 -0700

    terraform

--- Terraform/03-example/ec2.tf
+++ Terraform/03-example/ec2.tf
@@ -75,7 +75,7 @@ output "myfile" {
   resource "null_resource" "run_shell_script" {
     depends_on    = [aws_instance.sample]
     triggers = {
-      policy_sha1 = "${sha1(file(module.my_git_repo))}"
+      policy_sha1 = "${sha1(file(module.my_git_repo.hello.sh))}"
     }
     provisioner "remote-exec" {
       connection {
